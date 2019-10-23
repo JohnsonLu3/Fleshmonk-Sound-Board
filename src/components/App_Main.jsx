898235c20190b923df400b7021f39bde56321f4c
JohnsonLu3 <JohnsonLu94@gmail.com>
2019-10-22 21:21:31 -0400

    added world list

--- src/components/App_Main.jsx
+++ src/components/App_Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../scss/App.scss';
 import fleshmonk from '../image/fleshmonk.png';
-
+import List_Words from './List_Words.jsx';
 var state = {}
 
 class App_Main extends React.Component {
@@ -60,12 +60,19 @@ class App_Main extends React.Component {
 					<img id="fleshmonk_logo" src={fleshmonk} />
 				</div>
 				
+				<List_Words wordList = {clips} addWord={this.addWord}></List_Words>
 			</main>
 		);
 	}
 	
 	generate = () =>{
 		return;
+	}
+	
+	addWord = (word) =>{
+		console.log("add " + word);
+		let req_input = document.getElementById("req_input");
+		req_input.value = req_input.value + " " + word;
 	}
 }
 export default App_Main;
---
+++ src/components/List_Words.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import '../scss/wordList.scss';
+
+class List_Words extends React.Component  {
+		
+	constructor(props) {
+		super(props);	
+	}
+	
+	render(){
+		
+		const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+		return (
+			<div className="wordList">
+				<h2>available words</h2>
+				
+				<div className="flex">
+					{
+						letters.map((value, index) => {
+							return this.letterContainer(value, letters);
+						})
+					}
+				</div>
+			</div>
+		);
+	}
+	
+	letterContainer = (letter) =>{		
+		const list = this.props.wordList;
+		const addWord = this.props.addWord;
+		if(list != null){
+			return(
+				<div className="letterContainer">
+					<h3>{letter}</h3>	
+					<ul>
+						{
+							list.map((word, index) =>{
+								let start = word[0].toLowerCase();
+								if(start == letter){
+								   return (<li><button onClick={addWord.bind(this, word)}>{word}</button></li>)
+								}
+							})
+						}
+					</ul>
+				</div>
+			);
+		}else{
+			return;
+		}
+	}
+}
+export default List_Words;
